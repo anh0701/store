c9c97fd1d35a6277d78ae41710d2d660d5c1d102
Pham Thi Quynh Anh <quynhanhphamthi01@gmail.com>
2024-02-06 15:57:35 +0700

    add create volume

--- Server/run.sh
+++ Server/run.sh
@@ -1,29 +1,32 @@
 echo "Which option do you choose?"
 PS3="Select the operation: "
-select yn in "create compose file and run container" "run container" "stop and remove container created by up" "remove volume" "cancel"; do
+sudo  systemctl start docker
+select yn in "create compose file and run container" "run container" "stop and remove container created by up" "create volume" "remove volume" "cancel"; do
     case $yn in
         create\ compose\ file\ and\ run\ container ) 
             read -p "Enter the volumes name: " n1
             read -p "Enter the password mssql: " n2
             # echo "$n1 - $n2"
-            sudo  systemctl start docker
             sudo docker volume create $n1
             mkdir output/$n1
             cp .env mssql.yaml output/$n1
             sed -i 's/test-vol/'$n1'/g' output/$n1/mssql.yaml
-            sed -i 's/123456/'$n2'/g' output/$n1/.env
+            sed -i 's/YourStrong@Passw0rd/'$n2'/g' output/$n1/.env
             # cat output/$n1/mssql.yaml
             sudo docker-compose -f mssql.yaml up -d
             break
             ;;
         run\ container )
             read -p "Enter the path to compose file: " composeup
-            sudo  systemctl start docker
             sudo docker-compose -f $composeup up -d
             ;;
         stop\ and\ remove\ container\ created\ by\ up )
             read -p "Enter the path to compose file: " composedown
             sudo docker-compose -f $composedown down
+            ;;
+        create\ volume )
+            read -p "Enter the volume name: " volumecre
+            sudo docker volume create $volumecre
             ;;
         remove\ volume )
             read -p "Enter the volume name: " volume
